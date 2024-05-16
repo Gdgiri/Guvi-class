f7c802d55be6ed03528bb11dc64f5c2742a7fc49
Gdgiri <giridharan831@gmail.com>
2024-05-16 14:10:55 +0530

    update

--- Webkata/module2/DOM-10am day1/multiple/script.js
+++ Webkata/module2/DOM-10am day1/multiple/script.js
@@ -7,12 +7,15 @@ function element(tagname,content,attriname,attrivalue){
 }
 
 const h1=element("h1","Hi i am giridharan this was done by using function in DOM","class","h1");
+const h2=element("h1","2Hi i am giridharan this was done by using function in DOM","class","h1");
+const h3=element("h1","3Hi i am giridharan this was done by using function in DOM","class","h1");
 const ul=element("ul","","class","ul");
 const li=element("li","","class","li");
 const nav=element("nav","","class","nav");
 const header=element("header","","class","header");
 
-li.appendChild(h1);
+li.append(h1,h2,h3);
+
 ul.appendChild(li);
 header.appendChild(ul);
 document.body.append(header);
---
+++ Webkata/module2/HTML day5-10AM/script.js
@@ -0,0 +1,10 @@
+var fname=document.getElementById("firstname");
+
+console.log(fname);
+
+var btn=document.getElementById("btnt").value;
+console.log(btn)
+
+btn.addEventListener('click',()=>{
+    alert("mgthghhugui")
+});
